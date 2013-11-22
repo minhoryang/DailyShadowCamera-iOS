9a433e65ea186958ded507419eaa1aa8b123fca2
minhoryang <minhoryang@gmail.com>
2013-11-22 21:32:15 +0900

    Solved the problem!! (thanks to @Baekjoon)
    1. transparent overlay layer
    2. overlay frame
    3. viewWillAppear

--- kr.ac.sogang.cnu.dailyshadowcamera/CNUViewController.m
+++ kr.ac.sogang.cnu.dailyshadowcamera/CNUViewController.m
@@ -25,17 +25,24 @@
     imagePickerController.modalPresentationStyle = UIModalPresentationCurrentContext;
     imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
     imagePickerController.delegate = self;
+    imagePickerController.cameraViewTransform = CGAffineTransformIdentity;
     
     imagePickerController.showsCameraControls = NO;
     
     // xib overlay
     [[NSBundle mainBundle] loadNibNamed:@"OverlayView" owner:self options:nil];
-    self.overlayView.frame = imagePickerController.cameraOverlayView.frame;
+    self.overlayView.frame = imagePickerController.view.frame;
     imagePickerController.cameraOverlayView = self.overlayView;
     self.overlayView = nil;
     
     self.imagePickerController = imagePickerController;
     [self presentViewController:self.imagePickerController animated:YES completion:nil];
+    //    [self performSelector:@selector(functionA) withObject:nil afterDelay:0.50];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [self presentViewController:self.imagePickerController animated:animated completion:nil];
+    self.imagePickerController.cameraOverlayView = self.overlayView;
 }
 
 - (void)didReceiveMemoryWarning
